9d18e7c85b3ac7dc9627f3511eeaa0495ef70a97
古志强-吴钩 <Hellowugou@126.com>
2016-03-20 12:40:39 +0800

    添加浅薄

---
+++ GuTextField/GuTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuTextField
+//
+//  Created by demo on 16/3/20.
+//  Copyright © 2016年 GuZhiQiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuTextField/GuTextField/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  GuTextField
+//
+//  Created by demo on 16/3/20.
+//  Copyright © 2016年 GuZhiQiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
